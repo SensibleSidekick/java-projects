a493d61ffe1b3d299caae094eced1100971dd864
Cypress <kad2321@yahoo.com>
2024-09-24 11:04:39 -0400

    Studio complete

---
+++ control-flow-and-collections/studio/inheritance-studio/src/main/java/questions/CheckBox.java
@@ -0,0 +1,25 @@
+package questions;
+
+public class CheckBox extends Question {
+
+    public CheckBox(String question, Choice[] choiceArray) {
+        super(question, choiceArray);
+        setMaxResponses(getNumCorrect());
+    }
+
+    @Override
+    public String toString(){
+        String newline = System.lineSeparator();
+        return newline + "CHOOSE ONE OR MORE:" + newline + getQuestion() + newline + getFormattedChoices();
+    }
+
+    public int getNumCorrect() {
+        int numCorrect = 0;
+        for (Choice choice : getChoiceMap().values()) {
+            if (choice.isCorrect()) {
+                numCorrect ++;
+            }
+        }
+        return numCorrect;
+    }
+}
